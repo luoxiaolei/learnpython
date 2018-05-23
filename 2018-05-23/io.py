e8181f6a24659378a38f79358367d8dba22727b5
luoxiaolei <270656157@qq.com>
2018-05-23 09:41:11 +0800

    add the useage of io

---
+++ 2018-05-23/io.py
@@ -0,0 +1,22 @@
+import os
+
+read = open('test.txt');
+for each_line in read.readlines():
+	(num, char) = each_line.split(':',1);
+	print(num,end='');
+	print(char,end='');
+read.close();
+
+print();
+
+read = open('test.txt');
+for each_line in read.readlines():
+	try:
+		(num, char) = each_line.split(':');
+		print(num,end='');
+		print(char,end='');
+	except:
+		print('have a error')
+read.close();
+
+print(os.path.exists('test.txt'))
