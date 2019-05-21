9f6c26c3ca4020f7ce208043baca62f352feece7
luoxiaolei <270656157@qq.com>
2019-05-21 10:00:21 +0800

    update operator example

---
+++ 01Python Basics/Operator.py
@@ -0,0 +1,22 @@
+#! python3
+
+# Exponent
+print(2 ** 3) # 8
+
+# Modulus/remainder
+print(22 % 8) # 6
+
+# Interger division/floored quotient
+print(22 // 8) # 2
+
+# Division 
+print(22 / 8) # 2.75
+
+# Multiplication
+print(3 * 5) # 15
+
+# Subtraction
+print(5 - 2) # 3
+
+# Addition
+print(2 + 2) # 4
